cc8c09d8a372f5746667fba4e2f19362fdee65d6
zheyang616 <zhe.yang@stmartin.edu>
2024-03-19 10:50:25 -0700

    feat: add bubble sort method

---
+++ SortingProject.java
@@ -0,0 +1,38 @@
+/**
+ * SortingProject
+ */
+public class SortingProject {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 54, 21, 1, 58, 87, 89, 9000, 2, 1};
+
+        for(int value : arr){
+            System.out.print(" " + value);
+        }
+
+        BubbleSort(arr);
+        System.out.println();
+
+        for (int value : arr){
+            System.out.print(" " + value);
+        }
+    }
+
+    public static void BubbleSort(int[] arr){
+
+        int temp;
+
+        for(int i = 0; i < arr.length - 1; i++){
+            for(int j = 0; j < arr.length - i - 1; j++)
+            //swapping
+            if(arr[j] > arr[j + 1]){
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+//public static void Swap(){
+
+//}
